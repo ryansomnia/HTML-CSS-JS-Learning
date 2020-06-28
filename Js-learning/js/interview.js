85e5fce5e9c0ce41294602967cc6573f766958ac
heriyanto.sitorus <heriyantositorus14@gmail.com>
2020-06-29 00:23:00 +0700

    Latihan Filter, Map & Reduce

--- Js-learning/js/interview.js
+++ Js-learning/js/interview.js
@@ -222,3 +222,115 @@
 // }));
 
 // console.log('====================================');
+// let i = 0;
+// let s = 0;
+// let arr = [5, 7, 8, 3];
+
+// function f() {
+//     while (i < arr.length) {
+//         s = s + arr[i]
+//         i = i + 1
+
+//     }
+
+
+//     console.log(s);
+
+// }
+// f()
+
+
+// let x = [11, 10, 10, 5, 10, 15, 20, 10, 7, 11]
+
+// function foo(x, a, b, i, j) {
+//     let k = j;
+//     let ct = 0;
+//     while (k > i - 1) {
+//         if (x[k] <= b && !(x[k] <= a)) {
+//             ct = ct + 1
+
+//         }
+//         k = k - 1
+//     }
+
+
+//     return ct
+// }
+// console.log(foo(x, 8, 18, 3, 6));
+// console.log(foo(x, 10, 20, 0, 9));
+// console.log(foo(x, 8, 18, 6, 3));
+// console.log(foo(x, 20, 10, 0, 9));
+// console.log(foo(x, 6, 7, 8, 8));
+
+// function g(str) {
+//     let i = 0;
+//     let newStr = "";
+//     let panjang = str.length
+//     while (i < panjang - 1) {
+//         newStr = newStr + str[i + 1]
+//         i = i + 1;
+//     }
+
+//     return newStr
+
+// }
+
+// function f(str) {
+//     if (str.length == 0) {
+//         return "";
+//     } else if (str.length == 1) {
+//         return str
+
+//     } else {
+//         return f(g(str)) + str[0]
+//     }
+
+// }
+
+// function h(n, str) {
+//     while (n != 1) {
+//         if (n % 2 == 0) {
+//             n = n / 2;
+//         } else {
+//             n = 3 * n + 1
+//         }
+//         str = f(str)
+//     }
+//     return str;
+// }
+
+// function pow(x, y) {
+//     if (y == 0) {
+//         return 1;
+//     } else {
+//         return x * (pow(x, y - 1));
+
+//     }
+
+// }
+
+// console.log(h(1, "fruits"));
+// console.log(h(2, "fruits"));
+// console.log(h(5, "fruits"));
+// console.log(h(pow(2, 1000000000000000), "fruits"));
+// console.log(h(pow(2, 9831050005000007), "fruits"));
+
+function map(array, func) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(func(array[i]))
+
+    }
+    return result;
+}
+
+function questionable(func) {
+    return function (array) {
+        return map(array, func);
+    };
+
+}
+
+g = function (x) { return x * x };
+console.log(questionable(g, [1, 2, 3]));
+
